0cd21ae5bb615bdf55782948bbb851f16d487bd3
Abdurrahmon2700 <sabrin1998@inbox.ru>
2022-05-13 19:49:56 +0500

    no finished

---
+++ main.js
@@ -0,0 +1,15 @@
+// Call  info list
+let elInfoList = $("ul");
+
+kinolar.forEach(function (kino) {
+  let newLi = elCreateElement("li", "card col-3 col-sm-3", "");
+  let titleMovie = elCreateElement("p", "", `Name:  ${kino.title}`);
+  let movieJanr = elCreateElement("p", "", `Genres:  ${kino.genres}`);
+  let movieCast = elCreateElement("p", "", `Cast:  ${kino.cast}`);
+  let movieYear = elCreateElement("p", "", `Year:  ${kino.year}`);
+
+
+  newLi.append(titleMovie, movieJanr, movieCast, movieYear)
+  elInfoList.append(newLi)
+  console.log(titleMovie);
+});
